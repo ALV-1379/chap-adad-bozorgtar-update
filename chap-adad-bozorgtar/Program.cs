89f47b56abff4416f4f785bb4a6448d0c3ac2639
Mohamad alavi <Mohamad alavi@DESKTOP-9QCQADM>
2023-06-21 09:06:39 -0700

    Add project files.

---
+++ chap-adad-bozorgtar/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace chap_adad_bozorgtar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maximom = 0;
+            int[] adad = new int[10];
+            for (int i=0; i<10; i++)
+            {
+                Console.WriteLine($"adad {i+1} ra vared konid");
+                adad[i] = Convert.ToInt32(Console.ReadLine());
+
+                if (adad[i] > maximom)
+                {
+                    maximom = adad[i];
+
+                }
+            }
+            Console.WriteLine($"adad bozorg tar barabr {maximom}");
+
+            Console.ReadKey();
+        }   
+    }
+}
